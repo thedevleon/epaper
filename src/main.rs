c9257b6c325c5d53a7eb18191329cabb5f9ac0f2
Leon <12817042+thedevleon@users.noreply.github.com>
2024-08-22 09:44:15 +0200

    feat: partial refresh

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,24 @@ use esp_hal::{
     prelude::*,
     spi::{master::Spi, SpiMode},
     system::SystemControl,
+    time
 };
 use embedded_hal_bus::spi::ExclusiveDevice;
 use embedded_graphics::{
-    text::Text, text::TextStyle, geometry::Point, mono_font::MonoTextStyle, Drawable,
+    text::{Alignment, Text, TextStyle, TextStyleBuilder},
+    geometry::Point,
+    mono_font::MonoTextStyle,
+    Drawable,
 };
 use profont::PROFONT_24_POINT;
 use weact_studio_epd::{graphics::Display290BlackWhite, Color};
 use weact_studio_epd::{
-    graphics::DisplayRotation, 
+    graphics::{DisplayBlackWhite, DisplayRotation, buffer_len},
     WeActStudio290BlackWhiteDriver,
 };
 use display_interface_spi::SPIInterface;
+use heapless::String;
+use core::fmt::Write;
 
 #[entry]
 fn main() -> ! {
@@ -29,7 +35,7 @@ fn main() -> ! {
     let system = SystemControl::new(peripherals.SYSTEM);
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
     let clocks = ClockControl::max(system.clock_control).freeze();
-    let mut delay = Delay::new(&clocks);
+    let delay = Delay::new(&clocks);
 
     esp_println::logger::init_logger_from_env();
 
@@ -42,7 +48,7 @@ fn main() -> ! {
     let rst = io.pins.gpio22; // D4 / GPIO22
     let busy = io.pins.gpio23; // D5 / GPIO23
 
-    let mut spi_bus = Spi::new(peripherals.SPI2, 100.kHz(), SpiMode::Mode0, &clocks).with_pins(
+    let spi_bus = Spi::new(peripherals.SPI2, 100.kHz(), SpiMode::Mode0, &clocks).with_pins(
         Some(sclk),
         Some(mosi),
         NO_PIN,
@@ -70,18 +76,55 @@ fn main() -> ! {
     let mut driver = WeActStudio290BlackWhiteDriver::new(spi_interface, busy, rst, delay);
     let mut display = Display290BlackWhite::new();
     display.set_rotation(DisplayRotation::Rotate90);
-    driver.init().unwrap();
-    log::info!("Display initialized.");
 
-    // Write hello world
-    let black_style = MonoTextStyle::new(&PROFONT_24_POINT, Color::Black);
-    let _ = Text::with_text_style("Hello World!", Point::new(8, 40), black_style, TextStyle::default()).draw(&mut display);
-    
-    // Update display
+    let mut partial_display_bw =
+        DisplayBlackWhite::<64, 128, { buffer_len::<Color>(64, 128) }>::new();
+    partial_display_bw.set_rotation(DisplayRotation::Rotate90);
+
+    let mut now = 999;
+    driver.init().unwrap();
+
+    let style = MonoTextStyle::new(&PROFONT_24_POINT, Color::Black);
+
+    let mut string_buf = String::<30>::new();
+    write!(string_buf, "Time:\nInterval:").unwrap();
+    let _ = Text::with_text_style(&string_buf, Point::new(8, 40), style, TextStyle::default())
+        .draw(&mut display)
+        .unwrap();
+    string_buf.clear();
+
     driver.full_update(&display).unwrap();
 
+    let text_style = TextStyleBuilder::new().alignment(Alignment::Right).build();
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        let elapsed = now;
+        now = elapsed + 1;
+        let _ = write!(
+            string_buf,
+            "{:8.0}ms\n{}ms",
+            now,
+            elapsed
+        );
+
+        let _ = Text::with_text_style(&string_buf, Point::new(128, 32), style, text_style)
+            .draw(&mut partial_display_bw);
+        string_buf.clear();
+
+        driver
+            .fast_partial_update(&partial_display_bw, 56, 156)
+            .unwrap();
+
+        partial_display_bw.clear(Color::White);
+
+        if now > 10 {
+            break;
+        }
+    }
+
+    // put the display to sleep
+    driver.sleep().unwrap();
+
+    loop {
+        continue;
     }
 }
